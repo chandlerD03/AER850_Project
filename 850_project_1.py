93949589382b866855ad3e5538585e8e16a5eee4
chandlerD03 <128150050+chandlerD03@users.noreply.github.com>
2024-10-06 20:15:06 -0400

    Step 3
    Data is sorted into training and testing. Data is scaled as well.  Correlation matrix is made. Correlation is also tested inbetween output and input variable.

--- 850_project_1.py
+++ 850_project_1.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 #step 1: Data processing 
 #data is read from the file and converted into a datafrqamce using Panadas. 
@@ -44,9 +46,38 @@ plt.show()
 
 #step 3: Data Visualization 
 #first data must be organized in x and y since we are doing supervised learning
+#step 3: Correlation Analysis
 
-sns.heatmap(np.abs(corr_matrix))
+X_columns = df[['X', 'Y', 'Z']]  # Features (make sure these are columns in your DataFrame)
+y_column = df['Step']  # Target (make sure this is a column in your DataFrame)
 
+X_train, X_test, y_train, y_test = train_test_split(X_columns, y_column ,
+                                                    test_size = 0.2,
+                                                    random_state = 42);
+
+
+
+# Initialize the scaler
+my_scaler = StandardScaler()
+
+my_scaler.fit(X_train)
+scaled_data_train = my_scaler.transform(X_train)
+scaled_data_train_df = pd.DataFrame(scaled_data_train, columns=X_train.columns)
+
+scaled_data_test = my_scaler.transform(X_test)
+scaled_data_test_df = pd.DataFrame(scaled_data_test, columns=X_test.columns)
+
+corr_matrix = scaled_data_train_df.corr()
+sns.heatmap(np.abs(corr_matrix), cmap='rocket')
+plt.title('Correlation Heatmap')
+
+
+corr1 = y_train.corr(X_train['X'])
+print(corr1)
+corr2 = y_train.corr(X_train['Y'])
+print(corr2)
+corr3 = y_train.corr(X_train['Z'])
+print(corr3)
 
 
 
